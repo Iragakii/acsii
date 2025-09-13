1ada45c520fca2804aed507c8a9c7c246582a881
iragaki jc <jcgroup.vni@gmail.com>
2025-09-13 15:57:29 +0700

    alfa

--- acsii/src/main.tsx
+++ acsii/src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const container = document.getElementById('root')
+if (!container) throw new Error('Failed to find the root element')
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
     <BrowserRouter>
-     <App /></BrowserRouter>
-   
-  </StrictMode>,
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
