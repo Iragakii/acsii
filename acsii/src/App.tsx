fedb0d0fe5520efd7e6e0e57f35ed98c939ad2c0
iragaki jc <jcgroup.vni@gmail.com>
2025-09-13 15:24:32 +0700

    faoal

--- acsii/src/App.tsx
+++ acsii/src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage/LoginPage";
 import CameraPage from "./LoginPage/CameraPage";
 
 function App() {
   return (
-
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/camera" element={<CameraPage />} />
       </Routes>
-
   );
 }
 
